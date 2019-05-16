d1edea7fb1fa30b7981b83c83b71c661d51d1c8e
Angel <angurie_2008@hotmail.com>
2019-05-16 10:56:47 -0500

    significativo por favor

--- php/ValidaAcceso.php
+++ php/ValidaAcceso.php
@@ -1,60 +1,128 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+  <title>Sistema de Control Vehicular</title>
+  <link rel="stylesheet" type="text/css" href="./assets/css/style.css" media="all">
+</head>
+
+<body id="iniciar">
+  <div id="logo">
+        <a href="index.php">
+          Sistema de Control Vehicular
+        </a>
+  </div>
+  <div id="login" class="bloque">
+    <h3>Identificate</h3>
+    <form id="inicio" name="form1" method="post" action="ValidaAcceso.php">
+      <label>Username
+      <input name="username" type="text" id="username" />
+      </label>
+      <p>
+        <label>Password
+        <input name="password" type="text" id="password" />
+        </label>
+      </p>
+      <p> 
+        <label>
+        <input type="submit" name="Submit" value="Entrar" />
+        </label>
+      </p>
+    </form>
+  </div>
+</body>
+</html>
+
+
 <?php
 	session_start();
+	include("Conexion.php");
+	$Con = Conectar();
+	
+	
 	$username = $_POST['username'];
 	$password = $_POST['password'];
-	print("Username: ".$username."</br>");
-	print("Password: ".$password."</br>");
 	
-	include("conexion.php");
-	$conexion = Conectar();
-	$SQL = "SELECT * FROM Usuarios WHERE username = '$username';";
-	$query = Consulta($conexion,$SQL);
-	$n = mysqli_num_rows($query);
-	if($n == 0){
-		print("No se encontró ningun usuario");
-		header("Location: error.html");
+	//print("Userame: " . $username."<br>");
+	//print("Pass: " . $password."<br>");
+	
+	$SQL = "select * from usuarios where username = '$username';";
+	
+	$Consulta =Consulta($Con,$SQL);
+	$n = mysqli_num_rows($Consulta);
+	if($n==0)
+	{
+		echo "<div id='error'>
+			<p>Usuario no encontrado</p>
+		</div>";
 	}
-	else{
-		print("realiza validacion de contra</br>");
-		$fila = mysqli_fetch_row($query);
-		if($fila[1] == $password){
-			print("Contraseña correcta</br>");
-			$SQL4 = "UPDATE Usuarios SET intento = 0 WHERE username = '$username';";
-			Consulta($conexion,$SQL4);
-			if($fila[2] == 1){
-				print("Acceso concedido</br>");
-				$_SESSION['username'] = $username; //asigna valor a variable global
-				$_SESSION['validacion'] = True;
-				$_SESSION['tiempo'] = time();
-				header("Location: menuPrincipal.php");
-			}
-			else{
-				print("Usuario bloqueado</br>");
-				header("Location: error.html");
-			}
+	else
+	{
+		//print("Realizar validacion de contraseña <br>");
+		$fila=mysqli_fetch_row($Consulta);
+		if($fila[1]==$password){
+		//print("Contraseña correcta<br>");
+
+		if($fila[2]==1){
+			//print("Inicio de sesion correcto");
+			$SQL2="Update usuarios set intento='0' where username='$username';";
+			$Consulta2 =Consulta($Con,$SQL2);
+
+			$_SESSION['username']=$username;
+			$_SESSION['validacion']=True;
+			$_SESSION['tiempo']=time();
+			header("Location:MenuPrincipal.php");
 		}
 		else{
-			if($fila[2] == 1){
-				if($fila[3] > 2){
-					$SQL3 = "UPDATE Usuarios SET estado = 0, intento = 0 WHERE username = '$username'";
-					Consulta($conexion,$SQL3);
-					print("Se ha bloqueado tu acceso");
-					header("Location: error.html");
-				}
-				else{
-					print("Contraseña incorrecta</br>");
-					$intentos = $fila[3] + 1;
-					print("Intentos: ".$intentos."</br>");
-					$SQL2 = "UPDATE Usuarios SET intento = '$intentos' WHERE username = '$username';";
-					Consulta($conexion,$SQL2);
-					header("Location: error.html");
+			echo "<div id='error'>
+				<p>Usuario bloqueado</p>
+			</div>";
+			
+
+		}
+		
+		}
+		else{
+			        //print("Contraseña incorrecta <br>");
+			$intento=$fila[3];
+			$intentos=$intento+1;
+			$intentosRes=3-$intentos;
+			if($fila[2]==0){
+				//AQUI
+				//print("Tu usuario está bloqueado<br>");
+				//setcookie("intento",$intentosRes,time+5);
+				//header("Location:error.html");
+				echo "<div id='error'>
+					<p>Usuario bloqueado</p>
+				</div>";
+			}else{
+			if($intentos<3){
+				//AQUI
+					$SQL2="Update usuarios set intento='$intentos' where username='$username';";
+					$Consulta2 =Consulta($Con,$SQL2);
+					//print("Te quedan: ".$intentosRes. " intentos<br>");
+					echo "<div id='error'>
+						<p>Usuario o contraseña incorrecta quedan ".$intentosRes." intentos </p>
+					</div>";
+				}else{
+					//AQUI
+					//print("Tu usuario quedó Bloqueado");
+					$SQL2="Update usuarios set intento='0', estado='0' where username='$username';";
+					$Consulta2 =Consulta($Con,$SQL2);
+					//header("Location:error.html");
+					echo "<div id='error'>
+					<p>Usuario bloqueado</p>
+					</div>";
+
+					
 				}
 			}
-			else{
-				print("El usuario se encuentra bloqueado");
-				header("Location: error.html");
-			}
+			
 		}
 	}
-	Desconectar($conexion);
-?> 
+	
+	Desconectar($Con);
+	
+	
+	
+?>
