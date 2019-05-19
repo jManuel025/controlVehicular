7fd7fd028421261a69077153f6041b422b91472d
Angel <angurie_2008@hotmail.com>
2019-05-19 14:02:46 -0500

    Updates de Vehiculo

---
+++ php/FUVehiculos.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+    <form id="form1" name="form1" method="post" action="">
+    <label>IdVehiculo
+      <input name="id" type="text" id="id" />
+    </label>
+    <p>
+      <label>
+        <input name="buscar" type="submit" id="buscar" value="buscar" />
+      </label>
+    </p>
+  </form>
+    
+</body>
+</html>
+<?php
+if (isset($_POST['id'])) {
+    $id=$_POST['id'];
+    include("conexion.php");
+    $Con=Conectar();
+    $SQL="Select * FRom vehiculos where idvehiculo LIKE '$id';";
+    $Query=Consulta($Con,$SQL);
+    $array=mysqli_fetch_row($Query);
+    var_dump($array);
+  print("<form method='post' action='../php/PUVehiculos.php' enctype='multipart/form-data'>
+    <h1>Vehículos</h1>
+    <label>
+      <p>ID Vehiculo Antiguo:</p>
+      <input name='idVehiculoV' type='number' id='idVehiculoV' value='".$array[0]."'  required />
+    </label>
+    <label>
+      <p>ID Vehiculo Nuevo:</p>
+      <input name='idVehiculoN' type='number' id='idVehiculoN' value='".$array[0]."'  required />
+    </label>
+    <label>
+      <p>Propietario:</p>
+      <input name='propietario' type='text' id='propietario'  value='".$array[1]."' required />
+    </label>
+    <label>
+      <p>Placa:</p>
+      <input name='placa' type='text' id='placa'  value='".$array[2]."' required />
+    </label>
+    <label>
+      <p>Tipo:</p>
+      <input name='tipo' type='text' id='tipo'  value='".$array[3]."' required />
+    </label>
+    <label>
+      <p>Modelo:</p>
+      <input name='modelo' type='text' id='modelo'  value='".$array[4]."' required />
+    </label>
+    <label>
+      <p>Año:</p>
+      <input name='year' type='number' id='year'  value='".$array[5]."' required min='1900' max='2100' step='1' />
+    </label>
+    <div class='uso'>
+      <p>Uso:</p>
+      <div class='radio'>
+        <input name='uso' type='radio' value='Particular' id='particular'  />
+        <label for='particular'>Particular</label>
+        <input name='uso' type='radio' value='Comercial' id='comercial' />
+        <label for='comercial'>Comercial</label>
+      </div>
+    </div>
+    <label>
+      <p>Color:</p>
+      <input name='color' type='text' id='color'  value='".$array[7]."' required />
+    </label>
+    <label>
+      <p>Puertas:</p>
+      <input name='puertas' type='number' id='puertas'  value='".$array[8]."' min='1' max='4' />
+    </label>
+    <label>
+      <p>Marca:</p>
+      <input name='marca' type='text' id='marca'  value='".$array[9]."' />
+    </label>
+    <div class='uso'>
+      <p>Transmisión:</p>
+      <div class='radio'>
+        <input name='transmision' type='radio' value='Automático' id='auto' />
+        <label for='auto'>Automatico</label>
+        <input name='transmision' type='radio' value='Estándar' id='stan' />
+        <label for='stan'>Estandar</label>
+      </div>
+    </div>
+    <label>
+      <p>Capacidad de carga:</p>
+      <input name='capCarga' type='number' id='capCarga'  value='".$array[11]."' />
+    </label>
+    <label>
+      <p>Serie:</p>
+      <input name='serie' type='text' id='serie' value='".$array[12]."' />
+    </label>
+    <label>
+      <p>Numero de motor:</p>
+      <input name='numMotor' type='text' id='numMotor' value='".$array[13]."' />
+    </label>
+    <label>
+      <p>Línea:</p>
+      <input name='linea' type='text' id='linea' value='".$array[14]."' />
+    </label>
+    <label>
+      <p>Sublinea:</p>
+      <input name='sublinea' type='text' id='sublinea' value='".$array[15]."' />
+    </label>
+    <label>
+      <p>Cilindraje:</p>
+      <input name='cilindraje' type='number' id='cilindraje' value='".$array[16]."' />
+    </label>
+    <label>
+      <p class='select'>Combustible:</p>
+      <select name='combustible' id='combustible'>
+        <option>Gasolina</option>
+        <option>Diesel</option>
+        <option>Electrico</option>
+        <option>Otro</option>
+      </select>
+    </label>
+    <label>
+      <p>Origen:</p>
+      <input type='text' name='origen' id='origen'  value='".$array[18]."' />
+    </label>
+    <br>
+    <br>
+    <input type='submit' name='Submit' value='Cambiar' class='btnRegistra' />
+  </form>
+    ");
+  
+  
+  
+  Desconectar($Con);
+}
+?>
---
+++ php/PUVehiculos.php
@@ -0,0 +1,57 @@
+<?php 
+	$id = $_POST["idVehiculoV"];
+	$idVehiculoN = $_POST["idVehiculoN"];
+	$Propietario = $_POST["propietario"];
+	$Placa = $_POST["placa"];
+	$Tipo = $_POST["tipo"];
+	$Modelo = $_POST["modelo"];
+	$Year = $_POST["year"];
+	$Uso = $_POST["uso"];
+	$Color = $_POST["color"];
+	$Puertas = $_POST["puertas"];
+	$Marca = $_POST["marca"];
+	$Transmision = $_POST["transmision"];
+	$capCarga = $_POST["capCarga"];
+	$Serie = $_POST["serie"];
+	$numMotor = $_POST["numMotor"];
+	$Linea = $_POST["linea"];
+	$Sublinea = $_POST["sublinea"];
+	$Cilindraje = $_POST["cilindraje"];
+	$Combustible = $_POST["combustible"];
+	$Origen = $_POST["origen"];
+	
+	print("ID Vehiculo = ".$idVehiculoN."<br>");
+	print("Propietario = ".$Propietario."<br>");
+	print("Placa = ".$Placa."<br>");
+	print("Tipo = ".$Tipo."<br>");
+	print("Modelo = ".$Modelo."<br>");
+	print("Año = ".$Year."<br>");
+	print("Uso = ".$Uso."<br>");
+	print("Color = ".$Color."<br>");
+	print("Puertas = ".$Puertas."<br>");
+	print("Marca = ".$Marca."<br>");
+	print("Transmision = ".$Transmision."<br>");
+	print("Capacidad de Carga = ".$capCarga."<br>");
+	print("Serie = ".$Serie."<br>");
+	print("Numero de motor = ".$numMotor."<br>");
+	print("Linea = ".$Linea."<br>");
+	print("Sublinea = ".$Sublinea."<br>");
+	print("Cilindraje = ".$Cilindraje."<br>");
+	print("Combustible = ".$Combustible."<br>");
+	print("Origen = ".$Origen."<br>");
+	
+	//enviar instrucciones SQL al SMBD
+	include("conexion.php");
+	$Con = Conectar();
+	$SQL = "UPDATE vehiculos SET idVehiculo='$idVehiculoN',propietario='$Propietario',placa='$Placa',tipo='$Tipo',modelo='$Modelo',anio='$Year',uso='$Uso',color='$Color',puerta='$Puertas',marca='$Marca',transmision='$Transmision',capCarga='$capCarga',serie='$Serie',numMotor='$numMotor',linea='$Linea',sublinea='$Sublinea',cilindraje='$Cilindraje',combustible='$Combustible',origen='$Origen' WHERE idVehiculo ='$id';";
+	$cambio = Consulta($Con, $SQL);
+	if ($cambio == True){
+		print("Cambio exitoso");	
+	}
+	else{
+		
+		
+		print(" 0 tablas afectadas. Cambio fallido");	
+	}
+	Desconectar($Con);
+ ?>
