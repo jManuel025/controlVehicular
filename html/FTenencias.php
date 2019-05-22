10282f4004c77dc93fbe06170233c4b62e9c10be
Angel <angurie_2008@hotmail.com>
2019-05-21 21:01:17 -0500

    Updates fullllll

--- html/FTenencias.php
+++ html/FTenencias.php
@@ -2,17 +2,13 @@
 <div class="contenido">
   <form method="post" action="../php/PTenencias.php" enctype="multipart/form-data">
     <h1>Tenencias</h1>
-    <!-- <label>
-        <p>ID Tenencia:</p>
-        <input name="idTenencia" type="number" id="idTenencia" required/ disabled="disabled">
-      </label> -->
     <label>
       <p>Vehiculo:</p>
       <input name="vehiculo" type="number" id="vehiculo" required />
     </label>
     <label>
       <p>Año:</p>
-      <input name="year" type="date" id="year" required min="1900" max="2100" />
+      <input name="year" type="number" id="year" required min="1900" max="2100" />
     </label>
     <label>
       <p>Monto:</p>
