de3016724254b51cfb0075a45dad14fa96e8a9ec
Juan Manuel <mac.jmanuel@gmail.com>
2019-05-23 11:22:27 -0500

    LLave corregida

--- php/ValidaAcceso.php
+++ php/ValidaAcceso.php
@@ -43,13 +43,13 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 $key = $_FILES['llave'];
 
-$key['name'] = $username.".txt";
-$name = $key['name'];
-$location = "C:/xampp/htdocs/controlVehicular/php/llaves/";
+// $key['name'] = $username.".txt";
+// $name = $key['name'];
+// $location = "C:/xampp/htdocs/controlVehicular/php/llaves/";
 $tmp_name = $key['tmp_name'];
-move_uploaded_file($tmp_name, $location.$name);
-$location2 = $location.$username.".txt";
-$manejador = fopen($location2, "r");
+// move_uploaded_file($tmp_name, $location.$name);
+// $location2 = $location.$username.".txt";
+$manejador = fopen($tmp_name, "r");
 if($manejador){
 	while(!feof($manejador)){
 			$llave = fgets($manejador);
