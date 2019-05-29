ad242618315effc336525313b564110c52f6dd59
Angel <angurie_2008@hotmail.com>
2019-05-29 10:22:27 -0500

    Solucion final

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if ($_SESSION['validacion']) {
-    header("refresh:600;url=/html/cerrarSesion.php");
+    header("refresh:600;url=html/cerrarSesion.php");
 } else {
     header("Location: ./html/FAcceso.php");
 }
--- php/formatoVerificaciones.php
+++ php/formatoVerificaciones.php
@@ -2,7 +2,7 @@
   error_reporting(0);
   session_start();
   if ($_SESSION['validacion']) {
-      header("refresh:600;url=/html/cerrarSesion.php");
+      header("refresh:600;url=../html/cerrarSesion.php");
   } else {
       header("Location: ../html/FAcceso.php");
   }
